2a9b55933db6f9bc7b26619bcd18663ac776d6b2
Jure Perak <jure.perak@hotmail.com>
2021-09-17 09:43:41 +0200

    Added configuration for web API controllers
    Added support in Startup.cs for web API controller that are placed in Managemail.Web project. Forced https route.

--- ManagemailBackend/Managemail.Alpha/Startup.cs
+++ ManagemailBackend/Managemail.Alpha/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +17,13 @@ namespace Managemail.Alpha
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+            The AddControllers() extension method adds the services required to use Web API Controllers, and nothing more. 
+            So you get Authorization, Validation, formatters, and CORS for example, but nothing related to Razor Pages or view rendering. 
+            This is important as we are using only Web API not MVC in this project
+             */
+            var mvcBuilderService = services.AddControllers();
+            mvcBuilderService.PartManager.ApplicationParts.Add(new AssemblyPart(System.Reflection.Assembly.Load("Managemail.Web")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +34,14 @@ namespace Managemail.Alpha
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //This only maps controllers that are decorated with routing attributes - it doesn't configure any conventional routes
+                endpoints.MapControllers();
             });
         }
     }
---
+++ ManagemailBackend/Managemail.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Managemail.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Managemail.Web
+{
+    [ApiController]
+    [Route(StringConstants.DefaultApiRoute + "[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
